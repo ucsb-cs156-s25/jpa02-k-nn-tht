f0baebb78787e4bf0e8a83573e418fb8c48cc02c
Kenneth Tan <kennethtan@ucsb.edu>
2025-04-03 18:01:02 -0700

    xt - updated with actual developer name and team

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -20,7 +20,7 @@ public class Developer {
         // You may use just the name that is used on <https://bit.ly/cs156-s25-teams>
         // i.e. your first name, or your first and initial of last name
 
-        return "Chris G.";
+        return "Kenneth T.";
     }
 
     /**
@@ -30,7 +30,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "kennethtan";
     }
 
     /**
@@ -41,12 +41,12 @@ public class Developer {
     public static Team getTeam() {
         // TODO: Change this to your team name
         Team team = new Team("s25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        team.addMember("Kenneth");
+        team.addMember("Timothy");
+        team.addMember("Cheng");
+        team.addMember("Tuan");
+        team.addMember("Conner");
+        team.addMember("Nikunj");
         return team;
     }
 }
