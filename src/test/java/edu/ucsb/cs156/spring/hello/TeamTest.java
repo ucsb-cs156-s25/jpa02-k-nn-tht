6c265ebb26dda38c5da17a2fff6857aeec8d69a2
Kenneth Tan <kennethtan@ucsb.edu>
2025-04-03 22:49:16 -0700

    finished tests

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,7 +19,39 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_returns_correct_boolean() {
+        Team team2 = new Team("test-team");
+        assertEquals(team.equals(team), true);
+        team2.addMember("Kenneth");
+        assertEquals(team.equals(team2), false);
+        assertEquals(team.equals("j"), false);
+    }
+
+    // @Test
+    // public void hashCode_returns_correct_hashes() {
+    //     Team t1 = new Team();
+    //     t1.setName("foo");
+    //     t1.addMember("bar");
+    //     Team t2 = new Team();
+    //     t2.setName("foo");
+    //     t2.addMember("bar");
+    //     assertEquals(t1.hashCode(), t2.hashCode());
+    // }
+
+    @Test
+    public void hashCode_workaround(){
+        // instantiate t as a Team object
+        int result = team.hashCode();
+        int expectedResult = -1226298695;
+        assertEquals(expectedResult, result);
+
+    }
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
